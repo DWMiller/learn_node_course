a26f7eca2fbaf74b91ee8de2a248a50933a02f9d
David Miller <davidabc66@gmail.com>
2017-07-29 23:18:25 -0400

    lesson 26, account updating"

--- routes/index.js
+++ routes/index.js
@@ -48,4 +48,12 @@ router.post(
 
 router.get('/logout', authController.logout);
 
+router.get('/account', authController.isLoggedIn, userController.account);
+
+router.post(
+  '/account',
+  authController.isLoggedIn,
+  catchErrors(userController.updateAccount)
+);
+
 module.exports = router;
